c1e18b42b6479b0be6e11cc9b393c2cc3a821830
Kowloon walled city (coin) <chichunng@Chis-MacBook-Pro.local>
2016-07-09 16:52:39 +0800

    %1

---
+++ git-util/git-push.sh
@@ -0,0 +1,13 @@
+git config --global credential.helper cache
+git config --global credential.helper "cache --timeout=3600"
+git add *
+git commit -m "%1"
+git pull
+git push
+
+# -- other helper links --
+
+# http://stackoverflow.com/questions/5343068/is-there-a-way-to-skip-password-typing-when-using-https-on-github
+# git config --global credential.helper osxkeychain
+# git config --global credential.helper wincred
+# http://www.h-online.com/open/news/item/Git-1-8-0-can-access-Windows-and-GNOME-keyrings-1733879.html
