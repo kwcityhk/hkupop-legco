f962d969ada9e3f86dbac042a9a5e73c74271380
kwcityhk <kwcity.hk@Chis-MacBook-Pro.local>
2016-08-07 10:15:03 +0800

    start to get new data

---
+++ hkupop-2016-0803-started-analysis/LC2016_final_20160730_0803_v1_POP.R
@@ -0,0 +1,88 @@
+#install.packages("rio")
+
+#
+# 9th download not information of 
+
+# excel
+# tab and comma as delimited ...
+#
+
+# excel
+# - filter 
+
+# cross tab
+#
+# highlight 2 column (or all columns)
+# pivot table
+# - crosstab
+#
+
+# weight 
+#
+# R6
+# default no weight
+# 1 - 11
+# 1001 - Leung tien k ...
+# NA a lot - 7x4
+# weight ... 
+# varaible field setting
+# summary of weights ... 
+# head count is list 1 is now 11.9124 ...
+
+library("rio")
+legco2016_0803 <- import("/Users/kwcity.hk/my-git/hkupop-legco/hkupop.source.data.2016-08-03/LC2016_final_20160730_0803_v1_POP.csvy")
+head(legco2016_0803)            
+
+# reproducible - what you use and what is the script ???
+# R cannot read the spss file ...
+# sharable ...
+
+# archive ... why not have those archived???
+# weight ... calculation formulea ... (official ... voter population official only this)
+
+# officail voter by district age sex ... 
+# gender age education for general population
+
+# % ...
+
+# double click a number in the crosstab ...
+
+# annual ...
+
+# 30/7 ... 3/8 field work (4/8 published; 5/8 on web)
+# 
+
+# ... simple ... 
+
+# 2 district
+# full sample
+# not decided and decide 2 samples ...( later to predict for those decided)
+
+# R pop ... 
+
+# iNote ... 
+
+# 2/9 ... 4/9 last ... 
+
+# 2 sets to merge data ... 
+
+# link ...
+
+# hk data or gamma(
+  
+# hkdata.org
+# votsonar ... !!!
+
+# ... 5% too high error for strategic voting ... 
+
+# 4/9 ... report merge their raw data ...
+
+# telegram votsonar - bias (telegrapm, politics ... bias)
+# bias still get info and still 
+# adjust for hkupop
+# network survey (05/16 - 09/16) a lot of on the road (random street polls)
+# respondent driven sampling (experimental) and if success 
+
+# prediction market (wise market -> vote whom or guess winner is a different question)
+
+# 
