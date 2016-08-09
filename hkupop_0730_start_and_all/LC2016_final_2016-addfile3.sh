a4f7eba2b24b6b1cd94e9d95eba018bbf3a8058a
kwcityhk <kwcity.hk@Chis-MacBook-Pro.local>
2016-08-09 23:21:59 +0800

    psppire seems not ok ...

--- hkupop_0730_start_and_all/LC2016_final_2016-addfile3.sh
+++ hkupop_0730_start_and_all/LC2016_final_2016-addfile3.sh
@@ -8,15 +8,26 @@ ls *.sps
 # the saving or add file has switch to UNICODE to ASCII which is not possible with CHinese char.
 # need fixing
 
-pspp  --syntax-encoding=UTF8 LC2016_final_2016-addfile-all-analysis.SPS       -o output_addfile/LC2016_final_2016-addfile-all-analysis_v1_POP-output.pdf       -e output_addfile/LC2016_final_2016-addfile-all-analysis-error.txt 
-pspp  --syntax-encoding=UTF8 LC2016_final_2016-addfile-all-analysis.SPS       -o output_addfile/LC2016_final_2016-addfile-all-analysis_v1_POP-output.txt       -e output_addfile/LC2016_final_2016-addfile-all-analysis-error.txt  
+pspp --syntax-encoding=UTF8 LC2016_final_2016-addfile.SPS  -o output_addfile/LC2016_final_2016-addfile_v1_POP-output.pdf                    -e output_addfile/LC2016_final_2016-addfile-error.txt    
 
+echo "-0730_0803-"
 pspp-dump-sav LC2016_final_20160730_0803_v1_POP.sav 		> LC2016_final_20160730_0803_v1_POP.sav.pspp-dump-sav.txt
 tail LC2016_final_20160730_0803_v1_POP.sav.pspp-dump-sav.txt
+echo "--0730_0803--"
+
+echo "*current*"
 pspp-dump-sav LC2016_final_current_v1_POP.sav 				> LC2016_final_current_v1_POP.sav.pspp-dump-sav.txt
 tail LC2016_final_current_v1_POP.sav.pspp-dump-sav.txt
+echo "**current**"
+
+echo "#-minus-#"
 pspp-dump-sav LC2016_final_current-minus07xx0801_v1_POP.sav > LC2016_final_current-minus07xx0801_v1_POP.pspp-dump-sav.txt
 tail LC2016_final_current-minus07xx0801_v1_POP.pspp-dump-sav.txt
+echo "##-minus-##"
+
+pspp  LC2016_final_2016-addfile-all-analysis.SPS       -o output_addfile/LC2016_final_2016-addfile-all-analysis_v1_POP-output.pdf       -e output_addfile/LC2016_final_2016-addfile-all-analysis-error.txt 
+pspp  LC2016_final_2016-addfile-all-analysis.SPS       -o output_addfile/LC2016_final_2016-addfile-all-analysis_v1_POP-output.txt       -e output_addfile/LC2016_final_2016-addfile-all-analysis-error.txt  
+
 
 pspp  LC2016_final_2016-addfile-all-minus-analysis.SPS -o output_addfile/LC2016_final_2016-addfile-all-minus-analysis_v1_POP-output.pdf -e output_addfile/LC2016_final_2016-addfile-all-minus-analysis-error.txt  
 pspp  LC2016_final_2016-addfile-all-minus-analysis.SPS -o output_addfile/LC2016_final_2016-addfile-all-minus-analysis_v1_POP-output.txt -e output_addfile/LC2016_final_2016-addfile-all-minus-analysis-error.txt   
